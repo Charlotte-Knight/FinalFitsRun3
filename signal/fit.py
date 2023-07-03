2e5ebf0abee9846935ef30530a8c31614478c5f3
Matthew Knight <mdk16@ic.ac.uk>
2023-07-03 11:16:57 +0100

    Add support for fitting ranges

--- signal/fit.py
+++ signal/fit.py
@@ -9,22 +9,35 @@ def readEvents(filename):
   dataset = w.data("data")
   return x, dataset
 
-def main(in_file, out_file, function="Gaussian", order=1, plot_savepath=None):
+def main(in_file, out_file, function="Gaussian", order=1, fit_ranges=[], plot_savepath=None):
   x, dataset = readEvents(in_file)
+
+  x.setRange("Full", x.getMin(), x.getMax())
+  for i, r in enumerate(fit_ranges):
+    low, high = r.split(",")
+    low, high = float(low), float(high)
+    x.setRange(f"range{i}", low, high)
+  fit_ranges_str = ",".join([f"range{i}" for i, r in enumerate(fit_ranges)])
+
   datahist = ROOT.RooDataHist("datahist", "datahist", ROOT.RooArgList(x), dataset)
 
-  f = getattr(functions, function)
-  pdf, others = f(x, postfix="cat0", order=order)
-  params = pdf.getParameters(x)
-  pdf.fitTo(datahist, ROOT.RooFit.Save())
+  f = getattr(functions, function)(x, postfix="cat0", order=order, randomise=True)
+  f.pdf.Print()
+  f.pdf.selectNormalizationRange("Full", True)
+  #f.pdf.selectNormalizationRange(fit_ranges_str, True)
+  res = f.pdf.fitTo(datahist, ROOT.RooFit.Save(), ROOT.RooFit.Range(fit_ranges_str))
+  res.Print()
 
   if plot_savepath is not None:
-    plotting.signal_fit.plotFit(datahist, x, pdf, params, plot_savepath, xlim=(110, 140))
+    xlim = (x.getMin(), x.getMax())
+    #xlim = (115, 135)
+    plotting.signal_fit.plotFit(datahist, x, f.pdf, f.vars, plot_savepath, xlim=xlim)
 
   wsig = ROOT.RooWorkspace("wsig", "wsig")
   wsig.Import(datahist)
-  wsig.Import(pdf)
+  wsig.Import(f.pdf)
   wsig.writeToFile(out_file)
+  wsig.Print()
 
 if __name__=="__main__":
   import argparse
@@ -35,8 +48,9 @@ if __name__=="__main__":
   parser.add_argument("in_file")
   parser.add_argument("out_file")
   parser.add_argument("--function", "-f", type=str, default="Gaussian")
-  parser.add_argument("--order", type=int, default=1)
+  parser.add_argument("--order", "-o", type=int, default=1)
   parser.add_argument("--plot-savepath","-p", type=str, default=None)
+  parser.add_argument("--fit-ranges", type=str, nargs="+", default=[])
   args = parser.parse_args()
 
-  main(args.in_file, args.out_file, args.function, args.order, args.plot_savepath)
+  main(args.in_file, args.out_file, args.function, args.order, args.fit_ranges, args.plot_savepath)

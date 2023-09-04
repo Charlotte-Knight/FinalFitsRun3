f34b764bd8809973b8be36ff2041568d88d45d9a
Matthew Knight <mdk16@ic.ac.uk>
2023-09-04 14:08:42 +0100

    Fix roofit verbosoity argument

--- common/args_and_logging.py
+++ common/args_and_logging.py
@@ -23,7 +23,7 @@ def addLoggingArguments(parser):
   parser.add_argument("--verbose", "-v", type=int, default=1, choices=range(-2,3),
                       help="Set verbosity level for finalFits scripts: %s"%(", ".join(f"{key}={value}" for key,value in finalfits_verbose_dict.items())))
   parser.add_argument("--roofit-verbose", "-rv", type=int, default=0, choices=range(-2,4),
-                      help="Set verbosity level for RooFit: %s"%(", ".join(f"{key}={value}" for key,value in finalfits_verbose_dict.items())))
+                      help="Set verbosity level for RooFit: %s"%(", ".join(f"{key}={value}" for key,value in roofit_verbose_dict.items())))
 
 def applyLoggingArguments(args):
   ROOT.RooMsgService.instance().setGlobalKillBelow(getattr(ROOT.RooFit, roofit_verbose_dict[args.roofit_verbose]))
